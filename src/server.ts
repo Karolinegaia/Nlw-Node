16e25fc083a9fde59d78ffd1e0907d7864ffbfe3
Karoline Gaia Alexandre <drakarolinegaia@gmail.com>
2024-04-03 15:31:50 -0300

    Adding Prisma

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import fastify from "fastify";
 
 const app = fastify()
 
+app.get('/', () => {
+    return 'Hello'
+})
 app.listen({ port: 3000}).then(() => {
     console.log('HTTP serve running!')
 })
